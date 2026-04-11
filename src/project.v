cc3ace921f9269fa7c361f1e37dcccd16c6b4e69
sharanya304 <146364293+sharanya304@users.noreply.github.com>
2026-04-11 10:33:51 +0530

    Refactor tt_um_example module for halfadder

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_halfadder (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,11 +17,12 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    assign uo_out[0] = ui_in[0] ^ ui_in[1];
+    assign uo_out[1] = ui_in[0] & ui_in[1];
+    assign uo_out[7:2] = 6'b0;
+    
 
 endmodule
