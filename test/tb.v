7fe7459b3f8ff74370b8049a05deb2f6b362e406
sharanya304 <146364293+sharanya304@users.noreply.github.com>
2026-04-11 14:24:04 +0530

    Rename module from tt_um_halfadder to tt_um_spi_master

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_halfadder user_project (
+  tt_um_spi_master user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
